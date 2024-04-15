73b730fd8e71c0fd01360976cb506fd79ccd0e88
petros0979 <petrosmemiru@gmail.com>
2024-04-15 01:33:36 -0700

    good job

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+"""Defines a class City"""
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Module of a city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
